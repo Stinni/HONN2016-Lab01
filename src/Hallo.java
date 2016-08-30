e28f6a1bae5889437323a05ab4112330d1ced54e
Kristinn Heiðar Freysteinsson <krihfre@hotmail.com>
2016-08-30 03:58:19 +0000

    first commit

---
+++ src/Hallo.java
@@ -0,0 +1,26 @@
+
+import java.lang.Object;
+
+/**
+ * 24.08.16
+ * @author Kristinn Heiðar Freysteinsson
+ *
+ */
+
+/**
+ *
+ * Forritið Hallo (Hallo.java)
+ * skrifar heilræði á skjáinn
+ */
+
+public class Hallo extends Object
+{
+    public static void main (String[] args)
+    {
+        String heilraedi = "Gott er þeim sem glatt hafa sinni,\n" +
+                "guð sé með oss öllum hér inni.\n";
+
+        // Skrifa á skjá
+        System.out.println (heilraedi);
+    }
+}
